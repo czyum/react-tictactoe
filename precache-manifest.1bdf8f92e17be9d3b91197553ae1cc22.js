4f8d7e8cd15983c0a4491bcfe4777aa643c3e0e0
czyum <chethan27091999@gmail.com>
2020-12-27 22:11:38 +0530

    Updates

---
+++ precache-manifest.1bdf8f92e17be9d3b91197553ae1cc22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c59ad4a7a04082efffc707521c8c69c",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "1c76e333c319f0c9f500",
+    "url": "/react-tictactoe/static/css/2.a48854d9.chunk.css"
+  },
+  {
+    "revision": "f07cddfb8035e1349bbc",
+    "url": "/react-tictactoe/static/css/main.1d58bf54.chunk.css"
+  },
+  {
+    "revision": "1c76e333c319f0c9f500",
+    "url": "/react-tictactoe/static/js/2.31e7e6a6.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/react-tictactoe/static/js/2.31e7e6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07cddfb8035e1349bbc",
+    "url": "/react-tictactoe/static/js/main.f6f9b0cb.chunk.js"
+  },
+  {
+    "revision": "db82b3e8f146d9408a77",
+    "url": "/react-tictactoe/static/js/runtime-main.c287636c.js"
+  }
+]);
